4e7d0af96577f37759b07b28efb121ebcca995dc
Bruno Parga <brunoparga@gmail.com>
2022-07-01 13:33:21 +0400

    Make m8ball distributed

--- m8ball/m8ball.erl
+++ m8ball/m8ball.erl
@@ -7,7 +7,8 @@
 %%% CALLBACKS %%%
 %%%%%%%%%%%%%%%%%
 
-start(normal, []) -> m8ball_sup:start_link().
+start(normal, []) -> m8ball_sup:start_link();
+start({takeover, _OtherNode}, []) -> m8ball_sup:start_link().
 
 stop(_State) -> ok.
 
