58a8975af3f78b1dd08fddacad9701eb817f9a7f
Bruno Parga <brunoparga@gmail.com>
2022-07-01 13:14:23 +0400

    Implement the basic m8ball app

---
+++ m8ball/m8ball.erl
@@ -0,0 +1,18 @@
+-module(m8ball).
+-behaviour(application).
+-export([start/2, stop/1]).
+-export([ask/1]).
+
+%%%%%%%%%%%%%%%%%
+%%% CALLBACKS %%%
+%%%%%%%%%%%%%%%%%
+
+start(normal, []) -> m8ball_sup:start_link().
+
+stop(_State) -> ok.
+
+%%%%%%%%%%%%%%%%%
+%%% INTERFACE %%%
+%%%%%%%%%%%%%%%%%
+
+ask(Question) -> m8ball_server:ask(Question).
